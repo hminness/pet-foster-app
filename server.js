e68b8a5f343c8e2905c5e2e21db36247fb37825a
hminness <100871706+hminness@users.noreply.github.com>
2022-06-24 21:26:00 -0500

    adding monogodb

--- server.js
+++ server.js
@@ -3,3 +3,16 @@ const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
 const app = express();
 require('dotenv').config();
+
+const dbConnectionString = process.env.DB_STRING;
+
+let db,
+    dbName = 'sample_mflix',
+    collection;
+
+MongoClient.connect(dbConnectionString)
+    .then(client => {
+        console.log('Connected to db')
+        db = client.db(dbName);
+        collection = db.collection('movies')
+    })
