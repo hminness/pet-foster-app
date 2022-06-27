2ded17c2ad371e799081db533f21f970b009ead8
hminness <100871706+hminness@users.noreply.github.com>
2022-06-27 18:05:36 -0500

    Adding port

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
-const app = express();
-require('dotenv').config();
-
-const dbConnectionString = process.env.DB_STRING;
+const app = express()
+require('dotenv').config()
+const dbConnectionString = process.env.DB_STRING
 
 let db,
-    dbName = 'sample_mflix',
-    collection;
+    dbName = 'pet_data',
+    collection
 
 MongoClient.connect(dbConnectionString)
     .then(client => {
         console.log('Connected to db')
         db = client.db(dbName);
-        collection = db.collection('movies')
+        collection = db.collection('pet_info')
     })
+
+app.listen(process.env.PORT || PORT, ()=>{
+    console.log(`Server is online`)
+})
