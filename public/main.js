1104b5d95cf72db8be811039abb78fe4a5f2db92
hminness <100871706+hminness@users.noreply.github.com>
2022-06-27 19:42:05 -0500

    added content

--- public/main.js
+++ public/main.js
@@ -1,15 +1,44 @@
 //This is where you put the code that reaches out to the server to send info back and forth. Info can travel back and forth using fetches
 
-document.querySelector('#clickMe').addEventListener('click', makeReq)
+const update = document.querySelector('#update-button')
+const deleteButton = document.querySelector('#delete-button')
+const messageDiv = document.querySelector('#message')
 
-async function makeReq(){
+//Update Yoda quote to Darth Vader
+update.addEventListener('click', _ => {
+  fetch('/quotes', {
+    method: 'put',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      name: 'Darth Vader',
+      quote: 'I find your lack of faith disturbing.'
+    })
+  })
+  .then(res=> {
+      if (res.ok) return res.json()
+  })
+  .then(response=> {
+      console.log(response)
+  })
+})
 
-  const userName = document.querySelector("#userName").value;
-  const res = await fetch(`/api?student=${userName}`)
-  const data = await res.json()
-
-  console.log(data);
-  document.querySelector("#petNameIn").textContent = data.name
-  document.querySelector("#petSexIn").textContent = data.status
-  document.querySelector("#petAgeIn").textContent = data.currentOccupation
-}
+//Delete Darth Vader
+deleteButton.addEventListener('click', _ => {
+    fetch('/quotes', {
+      method: 'delete',
+      headers: { 'Content-Type': 'application.json' },
+      body: JSON.stringify({
+          name: 'Darth Vader'
+      })
+    })
+    .then(res => {
+        if (res.ok) return res.json()
+    })
+    .then(response => {
+        if (response === 'No quote to delete') {
+          messageDiv.textContent = 'No Darth Vader quote to delete'
+        } else {
+          window.location.reload(true)
+        }
+      })
+  })
--- server.js
+++ server.js
@@ -22,6 +22,50 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(cors())
 
+app.get('/', (req, res) => {
+    db.collection('quotes').find().toArray()
+      .then(quotes => {
+        res.render('index.ejs', { quotes: quotes })
+      })
+      .catch(/* ... */)
+  })
+app.post('/quotes', (req, res) => {
+quotesCollection.insertOne(req.body)
+    .then(result=> {
+        res.redirect('/')
+    })
+    .catch(error=> console.error(error))
+})
 app.listen(process.env.PORT || PORT, ()=>{
-    console.log(`Server is online`)
+console.log(`Server is online`)
 }) 
+app.put('/quotes', (req, res) => {
+quotesCollection.findOneAndUpdate(
+    { name: 'Yoda' },
+    {
+        $set: {
+        name: req.body.name,
+        quote: req.body.quote
+        }
+    },
+    {
+        upsert: true
+    }
+)
+.then(result => {e
+    res.json('Success')
+    })
+.catch(error => console.error(error))
+})
+  app.delete('/quotes', (req,res) => {
+    quotesCollection.deleteOne(
+        { name: req.body.name }
+    )
+    .then(result => {
+      if (result.deletedCount === 0) {
+          return res.json('No quote to delete')
+      }
+      res.json(`Deleted Darth Vader's quote`)
+     })
+    .catch(error => console.error(error))
+  })
