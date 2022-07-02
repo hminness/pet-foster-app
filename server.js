c8233236dccc9502f57bab807f44510904cad356
hminness <100871706+hminness@users.noreply.github.com>
2022-07-01 20:15:16 -0500

    fix errors

--- server.js
+++ server.js
@@ -15,40 +15,38 @@ MongoClient.connect(dbConnectionString)
         console.log('Connected to db')
         db = client.db(dbName);
         collection = db.collection('pet_info')
-    })
+        app.set('view engine','ejs')
+        app.use(express.static('public'))
+        app.use(express.urlencoded({extended:true}))
+        app.use(express.json())
+        app.use(cors())
 
-app.set('view engine','ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
-app.use(cors())
-
-app.get('/', async (req, res) => {
-    try {
-        res.render('index.ejs')
-    } catch (error) {
-        console.log(error)
-        // response.status(500).send(message: error.message)
-}
-// db.collection('quotes').find().toArray()
-//     .then(quotes => {
-//     res.render('index.ejs', { quotes: quotes })
-//     })
-//     .catch(/* ... */)
-})
-
-// app.post('/quotes', (req, res) => {
-// quotesCollection.insertOne(req.body)
-//     .then(result=> {
-//         res.redirect('/')
-//     })
-//     .catch(error=> console.error(error))
-// })
-app.listen(process.env.PORT || PORT, ()=>{
-console.log(`Server is online`)
-}) 
-// app.put('/quotes', (req, res) => {
-// quotesCollection.findOneAndUpdate(
+        app.get('/', (req, res) => {
+            db.collection('pets').find().toArray()
+            .then(pets => {
+                res.render('index.ejs', { pets: pets })
+            })
+            .catch(/* ... */)
+        })
+// app.get('/', async (req, res) => {
+//     try {
+//         res.render('index.ejs')
+//     } catch (error) {
+//         console.log(error)
+//         // response.status(500).send(message: error.message)
+// }
+        app.post('/pets', (req, res) => {
+        petsCollection.insertOne(req.body)
+            .then(result=> {
+                res.redirect('/')
+            })
+            .catch(error=> console.error(error))
+        })
+        app.listen(process.env.PORT || PORT, ()=>{
+        console.log(`Server is online`)
+        }) 
+// app.put('/pets', (req, res) => {
+// petsCollection.findOneAndUpdate(
 //     { name: 'Yoda' },
 //     {
 //         $set: {
@@ -65,8 +63,8 @@ console.log(`Server is online`)
 //     })
 // .catch(error => console.error(error))
 // })
-//   app.delete('/quotes', (req,res) => {
-//     quotesCollection.deleteOne(
+//   app.delete('/pets', (req,res) => {
+//     petsCollection.deleteOne(
 //         { name: req.body.name }
 //     )
 //     .then(result => {
@@ -77,3 +75,4 @@ console.log(`Server is online`)
 //      })
 //     .catch(error => console.error(error))
 //   })
+})
